66698ebce47a1bba4291e0606f12c7f1e8bf51fc
Scott Chamberlain <sachamber@fredhutch.org>
2024-12-11 10:21:25 -0800

    change output of rls_perform to a tibble upon success; remains error upon error

--- R/perform.R
+++ R/perform.R
@@ -1,12 +1,13 @@
 #' Perform a query
 #'
 #' @export
+#' @importFrom tibble tibble
 #' @param query an s3 object of class `privilege` or `row_policy, required.
 #' if `con` is not supplied, we attempt to get the connection
 #' from `query`; if it is not found we try to use a value passed to `con`.
 #' @param con DBI connection object, optional, see `query`
-#' @return error from PostgreSQL or Redshift upon error, or an integer
-#' value
+#' @return error from PostgreSQL or Redshift upon error; upon success a single
+#' row tibble with column "rows_affected" (integer)
 rls_perform <- function(query, con = NULL) {
   assert_is(query, c("privilege", "row_policy"))
   con <- as_con(query %||% con)
@@ -15,5 +16,6 @@ rls_perform <- function(query, con = NULL) {
     privilege = translate_privilege(query, con),
     row_policy = translate_row_policy(query, con)
   )
-  dbExecute(con, sql)
+  res <- dbExecute(con, sql)
+  tibble(rows_affected = res)
 }
